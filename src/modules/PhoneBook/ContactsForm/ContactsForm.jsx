e87d9ae0743b7cc0523f6160e7b3741cbf7f638d
Motkova Kateryna <sirenkoekaterina90@gmail.com>
2023-10-18 19:33:46 +0300

    fixed patterns

--- src/modules/PhoneBook/ContactsForm/ContactsForm.jsx
+++ src/modules/PhoneBook/ContactsForm/ContactsForm.jsx
@@ -33,7 +33,7 @@ return (
         name="name"
         value={name}
         placeholder="Add name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
       />
@@ -47,7 +47,7 @@ return (
         name="number"
         value={number}
         placeholder="Add phone number"
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
       />
